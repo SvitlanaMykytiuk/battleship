c3d89799d9aa9d3a1b1ded11070fc282b18ca09e
SvitlanaMykytiuk <svetlana.mikityuk@gmail.com>
2023-01-22 21:44:41 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ public class Main {
         FieldGenerator fieldGenerator = new FieldGenerator(10, 10, 1, 2, 3, 4);
         int[][] field = fieldGenerator.generate();
 
-//        Game game = new  Game (field, fieldGenerator.getLength(), fieldGenerator.getWidth(), 3);
-//        game.start();
+        Game game = new  Game (field, fieldGenerator.getLength(), fieldGenerator.getWidth(), 3);
+        game.start();
 
         FieldPrinter fieldViewer = new FieldPrinter(fieldGenerator.getLength());
         fieldViewer.printField(field);
